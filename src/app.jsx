2fd92708f9c99402d24fe3b4edb31fa29f75e263
Joe Mancera <joeharrymancera@gmail.com>
2023-08-02 20:14:06 -0500

    feat: add new buuton comonent and numerica component

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'preact/hooks'
 import './app.css'
 import { getTwitchClient, connect, onMessage, disconnect } from '../utils/twitchConnection'
+import { Button } from './components/button'
 
 export function App() {
   const [channelName, setChannelName] = useState('rothiotome')
   const [twitchClient, setTwitchClient] = useState(null)
+  const [isConnected, setIsConnected] = useState(false)
 
   const handleConnectClick = () => {
     const client = getTwitchClient(channelName)
@@ -13,23 +15,25 @@ export function App() {
       console.log(user.username, message)
     })
     setTwitchClient(client)
+    setIsConnected(true)
     console.log('Connected')
   }
 
   const handleDisconnectClick = () => {
     disconnect(twitchClient)
+    setIsConnected(false)
     console.log('Disconnected')
   }
   return (
     <>
       <h1>Numerica Combo Game</h1>
       <input type="text" value={channelName} onChange={(e) => setChannelName(e.target.value)} />
-        <button onClick={handleConnectClick}>
+        {!isConnected && <Button onClick={handleConnectClick}>
           Connect to Twitch
-        </button>
-        <button onClick={handleDisconnectClick}>
+        </Button>}
+        {isConnected && <Button onClick={handleDisconnectClick}>
           Disconnect from Twitch
-        </button>
+        </Button>}
     </>
   )
 }
---
+++ src/components/numerica.jsx
@@ -0,0 +1,5 @@
+export default function Numerica({ value }) {
+  return (
+    <div>{value}</div>
+  )
+}
