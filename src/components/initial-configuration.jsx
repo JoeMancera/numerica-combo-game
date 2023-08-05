4b36ea94f3c1af9ddaaa0686537cf108c0faf8f7
Joe Mancera <joeharrymancera@gmail.com>
2023-08-04 21:19:13 -0500

    feat: improve styles

--- src/components/initial-configuration.jsx
+++ src/components/initial-configuration.jsx
@@ -3,9 +3,10 @@ import { Button } from './button'
 export const InitialConfiguration = ({channelName, setChannelName, handleConnectClick}) => {
   return (
     <section className="flex flex-col gap-4">
-      <h1 className='text-red-600'>Numerica Combo Game</h1>
-      <label htmlFor="channelName">Channel Name
-        <input name="channelName" type="text" value={channelName} onChange={(e) => setChannelName(e.target.value)} />
+      <h1 className='text-red-600 drop-shadow-md'>Numerica Combo Game</h1>
+      <label className='flex flex-col text-left'>
+        <span className="pl-2">Channel Name</span>
+        <input className="text-black" name="channelName" type="text" value={channelName} onChange={(e) => setChannelName(e.target.value)} />
       </label>
       <Button className="bg-green-500" onClick={handleConnectClick}>
         Start game
