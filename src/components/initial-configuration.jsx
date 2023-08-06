73855b2e516e605fc97a0c88a710be5b5185f065
Joe Mancera <joeharrymancera@gmail.com>
2023-08-05 21:18:32 -0500

    fix styles title

--- src/components/initial-configuration.jsx
+++ src/components/initial-configuration.jsx
@@ -3,7 +3,8 @@ import { Button } from './button'
 export const InitialConfiguration = ({channelName, setChannelName, handleConnectClick}) => {
   return (
     <section className="flex flex-col gap-4">
-      <h1 className='text-red-600 drop-shadow-md'>Numerica Combo Game</h1>
+      <h1 className='flex flex-col text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-t from-purple-400 to-pink-600'>
+        <span>Numerica</span><span>Combo Game</span></h1>
       <label className='flex flex-col text-left'>
         <span className="pl-2">Channel Name</span>
         <input className="text-black" name="channelName" type="text" value={channelName} onChange={(e) => setChannelName(e.target.value)} />
--- src/components/numerica.jsx
+++ src/components/numerica.jsx
@@ -1,7 +1,7 @@
 export const Numerica = ({ value }) => {
   return (
     <div>
-      <span className='text-3xl text-red-500'>
+      <span className='animate-puls text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-t from-purple-400 to-pink-600'>
         {value}
       </span>
     </div>
