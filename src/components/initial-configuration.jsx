39cd5ac10b6b59ad1f31c2cddbcecfa0aeba4f2a
Joe Mancera <joeharrymancera@gmail.com>
2023-08-07 20:37:47 -0500

    refactor: improve styles

--- src/components/initial-configuration.jsx
+++ src/components/initial-configuration.jsx
@@ -3,13 +3,15 @@ import { Button } from './button'
 export const InitialConfiguration = ({channelName, setChannelName, handleConnectClick}) => {
   return (
     <section className="flex flex-col gap-4">
-      <h1 className='flex flex-col text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-t from-purple-400 to-pink-600'>
-        <span>Numerica</span><span>Combo Game</span></h1>
+      <h1 className='flex flex-col text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-t from-purple-400 to-pink-600 stroke'>
+        <span>Numerica</span>
+        <span>Combo Game</span>
+      </h1>
       <label className='flex flex-col text-left'>
         <span className="pl-2">Channel Name</span>
-        <input className="text-black" name="channelName" type="text" value={channelName} onChange={(e) => setChannelName(e.target.value)} />
+        <input className="text-gray-700" name="channelName" type="text" value={channelName} onChange={(e) => setChannelName(e.target.value)} />
       </label>
-      <Button className="bg-green-500" onClick={handleConnectClick}>
+      <Button className="bg-green-400 text-gray-600 hover:text-white" onClick={handleConnectClick}>
         Start game
       </Button>
     </section>
--- src/components/numerica.jsx
+++ src/components/numerica.jsx
@@ -1,9 +1,10 @@
-export const Numerica = ({ value }) => {
+export const Numerica = ({ value, combo }) => {
   return (
-    <div>
-      <span className='animate-puls text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-t from-purple-400 to-pink-600'>
+    <div className="flex flex-col">
+      <span className='animate-puls text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-t from-purple-400 to-pink-600 stroke'>
         {value}
       </span>
+      <span>+{combo}1</span>
     </div>
   )
 }
