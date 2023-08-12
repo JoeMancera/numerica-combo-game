56f515854bebd5dd34ea9f7918faadc262d2ee66
Joe Mancera <joeharrymancera@gmail.com>
2023-08-11 22:31:01 -0500

    feat: add list of combo messages

--- utils/common.js
+++ utils/common.js
@@ -1,3 +1,18 @@
 export const comboMessageList = [
   'rothioStabanim',
+  'afordiThinking',
+  'psuzumPokerface',
+  'ildesiChurro',
+  'altaskCode',
+  'sailor395UwU',
+  'meleni4CABREADO',
+  'algebr3Driver',
+  'carmen67Jeje',
+  'sensei9Facepalm',
+  'catisa1Demon',
+  'ransilMAL',
+  'niv3keTodook',
+  'isaacs1Leak',
+  'papajo8Heart',
+  'kerk11F',
 ]
