1292fecbfabc398e3211a05c0f3b311255235da9
Joe Mancera <joeharrymancera@gmail.com>
2023-08-12 17:34:48 -0500

    improve: change ui game over

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -8,12 +8,16 @@ export const Game = ({value, user, showStopGame, combo, userGameOver, handleDisc
   return (
     <section className='flex flex-col justify-center'>
       <span className='stroke-dark text-lg'>High Score:{isNaN(highScore) ? 0 : highScore} </span>
-      {highScoreUser && <span className="text-orange-300">By {highScoreUser}</span>}
+      {highScoreUser && <span className="text-orange-300 text-xs">By {highScoreUser}</span>}
       <Numerica value={value} combo={combo} />
-      {user ? <span className="pl-2 text-orange-400">{user}</span> : <span className="pl-2 text-orange-400">
+      {user ? <span className="pl-2 text-orange-400 text-sm">{user}</span> : <span className="pl-2 text-orange-400">
         Let's go!
       </span> }
-      {userGameOver && <span className="text-red-400 stroke-dark">{userGameOver} lost!</span>}
+      {userGameOver && (
+        <div className="flex flex-col gap-1">
+          <span className="text-red-600 stroke-cyan text-lg">Game over!</span>
+          <span className="text-red-600 stroke-black text-sm">{userGameOver}</span>
+        </div>)}
       {showStopGame && <Button className="bg-red-600 text-white" onClick={handleDisconnectClick}>
         Stop Game
       </Button>}
