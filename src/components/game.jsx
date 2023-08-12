d58a6ed0fa50496e6fbfbcbf4ad6ea13454d699a
Joe Mancera <joeharrymancera@gmail.com>
2023-08-11 20:01:20 -0500

    feat: improve the numerica component

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -2,13 +2,13 @@ import { Numerica } from './numerica'
 import { Button } from './button'
 
 export const Game = ({value, user, showStopGame, handleDisconnectClick, combo}) => {
-  const highScore = sessionStorage.getItem('x-highScore-numerica')
+  const highScore = parseInt(sessionStorage.getItem('x-highScore-numerica'))
   const highScoreUser = sessionStorage.getItem('x-highScore-numerica-user')
   
   return (
     <section className='flex flex-col justify-center'>
-      <span>High Score:{parseInt(highScore) ?? 0} </span>
-      <span className="text-orange-300">By {highScoreUser}</span>
+      <span>High Score:{isNaN(highScore) ? 0 : highScore} </span>
+      {highScoreUser && <span className="text-orange-300">By {highScoreUser}</span>}
       <Numerica value={value} combo={combo} />
       {user ? <span className="pl-2 text-orange-400">{user}</span> : <span className="pl-2 text-orange-400">
         Let's go!
