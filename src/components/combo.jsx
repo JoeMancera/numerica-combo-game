9e574932a2b8aa0e486c5aa7097fcb96ca585878
Joe Mancera <joeharrymancera@gmail.com>
2023-08-11 23:23:03 -0500

    feat: add text color for the combos

--- src/components/combo.jsx
+++ src/components/combo.jsx
@@ -2,8 +2,9 @@ import { useAnimation } from '../hooks/use-animation'
 
 export const Combo = ({value}) => {
   const { animation } = useAnimation(value)
+  const comboTextColor = value < 6 ? 'text-cyan-300' : value < 12 ? 'text-orange-300' : value < 18 ? 'text-orange-500' : 'text-red-600'
   return (
-  <span className={`${animation ? 'shake-left-right text-white' : '' } flex absolute top-0 right-0 animate-pulse text-orange-300`}>
+  <span className={`${animation ? 'shake-left-right text-white' : '' } flex absolute top-0 right-0 animate-pulse ${comboTextColor}`}>
     +{value}
   </span>
   )
