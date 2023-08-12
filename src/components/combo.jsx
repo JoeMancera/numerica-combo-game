401f4d2e71cd2a6ce3f24bf79d847b052eab039f
Joe Mancera <joeharrymancera@gmail.com>
2023-08-12 18:17:59 -0500

    feat: improve the color text combo

--- src/components/combo.jsx
+++ src/components/combo.jsx
@@ -2,7 +2,7 @@ import { useAnimation } from '../hooks/use-animation'
 
 export const Combo = ({value}) => {
   const { animation } = useAnimation(value)
-  const comboTextColor = value < 6 ? 'text-cyan-300' : value < 12 ? 'text-orange-300' : value < 18 ? 'text-orange-500' : 'text-red-600'
+  const comboTextColor = value < 3 ? 'text-cyan-300' : value < 6 ? 'text-orange-300' : value < 9 ? 'text-orange-500' : 'text-red-600'
   return (
   <span className={`${animation ? 'shake-left-right text-white' : '' } flex absolute top-0 right-0 animate-pulse ${comboTextColor}`}>
     +{value}
